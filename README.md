7dcf74485621f1d306657e49cb44d8e90eae072a
l0snight <rainbowzhang00@gmail.com>
2020-04-09 00:17:20 +0800

    Update install.sh and README

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 1. **Clone Repository**
 ```
 git clone https://https://github.com/L0snight/subconvert-config.git
-git clone https://github.com/L0snight/Rules.git --recursive
 ```
 2. **Install**
 ```
--- install.sh
+++ install.sh
@@ -1,4 +1,45 @@
 #!/bin/bash
 
+cd `dirname $0`
 
+function assert() {
+  "$@"
+  if [ $? != 0 ];then
+    echo "Command $@ failure"
+    exit 1
+  fi
+}
 
+function assert_command() {
+  if !which $1 > /dev/null 2> /dev/null;then
+    echo "$2"
+  fi
+}
+
+function _build() {
+  assert_command git 'git not found'
+  assert  git clone https://github.com/L0snight/Rules.git --recursive
+  echo "Clone rules repo"
+}
+
+function _install() {
+  echo "Install"
+}
+
+function _help() {
+  echo "help"
+}
+
+case "$1" in
+  "build")
+  shift
+  _build "$@"
+  ;;
+  "install")
+  shift
+  _install "$@"
+  ;;
+  *)
+  _help
+  ;;
+esac
